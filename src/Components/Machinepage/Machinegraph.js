7fb6ec2d9bfd26fd06d9026a14d97d6fe5cb03a1
klerm <jeremeymain@gmail.com>
2021-11-03 03:36:07 +0100

    You can see de graphs but cant interact with them also isnt responsive yet. user can see the graph tho but can use some css fine tuning

--- src/Components/Machinepage/Machinegraph.js
+++ src/Components/Machinepage/Machinegraph.js
@@ -7,80 +7,149 @@ import {
   CartesianGrid,
   Tooltip,
   Legend, 
-  ResponsiveContainer 
 } from 'recharts';
 
 
 const data = [
   {
     name: "0:00",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
+    Uptime: 20,
+    Downtime: 10,
   },
   {
-    name: "0:00",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
+    name: "1:00",
+    Uptime: 30,
+    Downtime: 0,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
+    name: "2:00",
+    Uptime: 30,
+    Downtime: 0,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
+    name: "3:00",
+    Uptime: 30,
+    Downtime: 0,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
+    name: "4:00",
+    Uptime: 30,
+    Downtime: 0,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
+    name: "5:00",
+    Uptime: 30,
+    Downtime: 0,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
+    name: "6:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "7:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "8:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "9:00",
+    Uptime: 10,
+    Downtime: 20,
+  },
+  {
+    name: "10:00",
+    Uptime: 0,
+    Downtime: 30,
+  },
+  {
+    name: "11:00",
+    Uptime: 15,
+    Downtime: 15,
+  },
+  {
+    name: "12:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "13:00",
+    Uptime: 25,
+    Downtime: 5,
+  },
+  {
+    name: "14:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "15:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "17:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "18:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "19:00",
+    Uptime: 20,
+    Downtime: 10,
+  },
+  {
+    name: "20:00",
+    Uptime: 10,
+    Downtime: 20,
+  },
+  {
+    name: "21:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "22:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
+  {
+    name: "23:00",
+    Uptime: 30,
+    Downtime: 0,
+  },
 ];
 
 function Machinegraph() {
   return (
-    <ResponsiveContainer width="70%" height="70%">
-      <BarChart
-        width={48}
-        height={30}
-        data={data}
-        barCategoryGap="-1%"
-        barSize = "30px"
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" stackId="a" fill="#369E32" />
-        <Bar dataKey="uv" stackId="a" fill="#D23333" />
-      </BarChart>
-  </ResponsiveContainer>
+    <BarChart
+    width={270}
+    height={160}
+    data={data}
+    barCategoryGap="-1%"
+    barSize = "30px"
+    margin={{
+      top: 20,
+      left:-35,
+      bottom: 5
+    }}
+  >
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="name" />
+    <YAxis />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="Uptime" stackId="a" fill="#369E32" />
+    <Bar dataKey="Downtime" stackId="a" fill="#D23333" />
+  </BarChart>
   );
 }
 
--- src/Components/Machinepage/Machinemonitoring.js
+++ src/Components/Machinepage/Machinemonitoring.js
@@ -3,83 +3,132 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Box from '@mui/material/Box';
-//import { border } from "@mui/system";
+import Graph from './Machinegraph';
+//import { CardHeader,Card,CardContent} from '@mui/material';
 
-/*
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  border: '3px solid'
-}));
-*/
+
 const settings = {
   infinite: true,
   slidesToShow: 4,
-  speed: 100,
+  speed: 10,
   rows: 4,
   slidesToshow: 1,
 };
-
-
 
 function Machinemonitoring() {
   return (
     <div style={{color: '#D3E2EA',height: '997px',}}>
       <Box  sx={{
+        height: '997px',
         width: 'auto',
         backgroundColor: '#2B2B34',
-        marginLeft: '20%',
+        marginLeft: '10%',
+        marginRight: '10%',
       }}>
           <Slider {...settings}>
-          <div style={{fontSize: '20px'}}>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade1</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
           </div>
-          <div>
-            <h3>Sawblade</h3>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade</h6>
+            <Graph/>
+          </div>
+          <div style= {{width: "100px", border: '3px solid red'}}>
+            <h6 style={{textAlign: 'center'}}>Sawblade20</h6>
+            <Graph/>
           </div>
           </Slider>
         </Box>
