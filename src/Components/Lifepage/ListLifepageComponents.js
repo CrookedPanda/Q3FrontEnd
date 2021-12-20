6c42d717995c78da3ffead681a952f0c735216d7
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-12-20 15:28:40 +0100

    Separate search bar from items and add a scrollbar

--- src/Components/Lifepage/ListLifepageComponents.js
+++ src/Components/Lifepage/ListLifepageComponents.js
@@ -19,34 +19,36 @@ const Item = styled(Paper)(({ theme }) => ({
 function LifepageComponents(lifepageComponents) {
   const {data} = lifepageComponents;
   return (
-    <TableContainer Component={Item}>
-        <Table sx={{ minWidth: 900 }} style={{display: "flex", flexDirection: "column"}} aria-label="simple table">
-          <TableHead>
-            <TableRow style={{display: "flex"}}>
-              <TableCell style={{color: '#D3E2EA', fontSize: '35px', flex: 1}}>Components</TableCell>
-              <TableCell style={{color: '#D3E2EA', fontSize: '35px', flex: 1}}>Actions</TableCell>
-              <TableCell style={{color: '#D3E2EA', flex: 1}}></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {data.map((lifepageComponents) => {
-            let used = lifepageComponents.totalActions;
-            let remaining = lifepageComponents.maxActions - lifepageComponents.totalActions;
-            return (
-              <TableRow key={lifepageComponents} style={{display: "flex"}}> 
-                <TableCell style={{color: '#D3E2EA', fontSize: '25px', display: "flex", flex: 1}}>{lifepageComponents.description}</TableCell>
-                <TableCell style={{color: '#D3E2EA', fontSize: '25px', display: "flex", flex: 1}} align="middle">{'Action: '+lifepageComponents.totalActions+'/'+lifepageComponents.maxActions}</TableCell>
-                <TableCell style={{color: '#D3E2EA', fontSize: '25px', display: "flex", flex: 1}} align="middle">
-                  <div style={{flex: used, backgroundColor: "#D23333"}}></div>
-                  <div style={{flex: remaining, backgroundColor: "#227A1E"}}></div>
-                </TableCell>
-              </TableRow>   
-            );
-          })}
-          </TableBody>
-          <TableRow style={{color: '#D3E2EA', fontSize: '25px', height: '75px'}} text-align="center">{"Zoekbalk"}</TableRow>
-        </Table>
-      </TableContainer>
+    <div style={{display: "flex", height: "90vh", flexDirection: "column"}}>
+      <TableContainer Component={Item} style={{display: "flex", flexDirection: "column", flex: 1, overflowY: "scroll"}}>
+          <Table sx={{ minWidth: 900 }} style={{display: "flex", flex: 1, flexDirection: "column"}} aria-label="simple table">
+            <TableHead>
+              <TableRow style={{display: "flex", marginTop: "2rem"}}>
+                <TableCell style={{color: '#D3E2EA', fontSize: '35px', flex: 1}}>Components</TableCell>
+                <TableCell style={{color: '#D3E2EA', fontSize: '35px', flex: 1}}>Actions</TableCell>
+                <TableCell style={{color: '#D3E2EA', flex: 1}}></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {data.map((lifepageComponents) => {
+              let used = lifepageComponents.totalActions;
+              let remaining = lifepageComponents.maxActions - lifepageComponents.totalActions;
+              return (
+                <TableRow key={lifepageComponents} style={{display: "flex"}}> 
+                  <TableCell style={{color: '#D3E2EA', fontSize: '25px', display: "flex", flex: 1}}>{lifepageComponents.description}</TableCell>
+                  <TableCell style={{color: '#D3E2EA', fontSize: '25px', display: "flex", flex: 1}} align="middle">{'Action: '+lifepageComponents.totalActions+'/'+lifepageComponents.maxActions}</TableCell>
+                  <TableCell style={{color: '#D3E2EA', fontSize: '25px', display: "flex", flex: 1}} align="middle">
+                    <div style={{flex: used, backgroundColor: "#D23333"}}></div>
+                    <div style={{flex: remaining, backgroundColor: "#227A1E"}}></div>
+                  </TableCell>
+                </TableRow>   
+              );
+            })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <div style={{color: '#D3E2EA', fontSize: '25px', padding: "1rem", borderColor: "white", borderWidth: 0, borderTopWidth: 1, borderStyle: "solid"}} text-align="center">{"Zoekbalk"}</div>
+      </div>
   );
 }
 
