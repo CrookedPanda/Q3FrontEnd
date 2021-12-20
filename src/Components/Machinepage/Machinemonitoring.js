f7fcd2e1354fa458521a1404bd7cf224d5eedeaa
Ramon Peeters <70950645+RamonPeeters@users.noreply.github.com>
2021-12-20 13:45:25 +0100

    Use new library for graph

--- src/Components/Machinepage/Machinemonitoring.js
+++ src/Components/Machinepage/Machinemonitoring.js
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Box from '@mui/material/Box';
-import Graph from './Machinegraph';
+import MachineGraph from './Machinegraph';
 import {useDispatch ,useSelector } from "react-redux";
 import axios from "axios";
 import {setMachines} from "../../Redux/Actions/MachineActions";
@@ -28,10 +28,44 @@ const settings = {
     const machines = useSelector((state) => state.allMachines.machines);
 
     const getmachines = async () => {
-      const response = await axios.get('https://localhost:44374/api/Poorten/machine/').catch((er) => {
-        console.log("Err,err")
-      });
-      dispatch(setMachines(response.data));
+      //const response = await axios.get('https://localhost:44374/api/Poorten/machine/').catch((er) => {
+      //  console.log("Err,err")
+      //});
+      //dispatch(setMachines(response.data));
+
+      var dataset = [
+        {
+            name: "Hi",
+            data: [
+                {
+                    startTime: "2021-12-13T12:00:00Z",
+                    endTime: "2021-12-14T00:00:00Z",
+                    type: "off"
+                },
+                {
+                    startTime: "2021-12-14T00:00:00Z",
+                    endTime: "2021-12-14T11:00:00Z",
+                    type: "on"
+                },
+                {
+                    startTime: "2021-12-14T11:00:00Z",
+                    endTime: "2021-12-14T12:00:00Z",
+                    type: "off"
+                },
+                {
+                    startTime: "2021-12-14T12:00:00Z",
+                    endTime: "2021-12-14T13:00:00Z",
+                    type: "on"
+                }
+            ]
+        }
+      ];
+
+      for (let i = 0; i < 31; i++) {
+          dataset.push(dataset[0]);
+      }
+
+      dispatch(setMachines(dataset));
     }
 
     useEffect(()=> {
@@ -45,30 +79,25 @@ const settings = {
     //}
 
     return (
-    <div style={{color: '#D3E2EA',height: '800px',}}>
-      <Box  sx={{
-        height: '800px',
-          
-        backgroundColor: '#2B2B34',
-        marginLeft: '10%',
-        marginRight: '10%',
-      }}>
-          <Slider {...settings}>
-          {machines.map(machine => (
-          <div key={machine.name} style= {{height: "px" ,width: "200px"}}>
-          <h6 style={{textAlign: 'center'}}>{machine.name}</h6>
-          <Link to={{
-            pathname: `/Machinecomp/${machine.name}`,
-          }}
-          >
-          <Graph data ={machine.uptime}/>
-          </Link>
+        <div style={{color: '#D3E2EA'}}>
+            <Box sx={{  
+            backgroundColor: '#2B2B34',
+            marginLeft: '10%',
+            marginTop: '2%',
+            marginRight: '10%' }}>
+                <Slider {...settings}>
+                    {machines.map(machine => (
+                        <div key={machine.name}>
+                            <h6 style={{textAlign: 'center'}}>{machine.name}</h6>
+                            <Link to={{pathname: `/Machinecomp/${machine.name}`}} style={{display: "flex", justifyContent: "center"}}>
+                                <MachineGraph items={machine.data}/>
+                            </Link>
+                        </div>
+                    ))}
+                </Slider>
+            </Box>
         </div>
-          ))}
-          </Slider>
-        </Box>
-    </div>
-    )
+    );
 }
 
 export default Machinemonitoring
